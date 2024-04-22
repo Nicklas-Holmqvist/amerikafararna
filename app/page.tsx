17ab7e1745a45736dc81ddeaee2b18db93f9a469
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2024-04-22 20:02:36 +0200

    fix: added hex code for hyphen

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
         Andersson Sandberg startade som även driver bloggen Glömd Värld i Marks
         härad. Det hela började med att Nicklas ville skriva om en amerikafarare
         till bloggen och han började då kika närmare på Älekulla socken. Efter
-        några dagar registrerande skapades en lista på över 400 rader och
+        några dagars registrerande skapades en lista på över 400 rader och
         personer som emigrerat till och från Amerika.
       </p>
       <p className="w-[80ch] pb-10">
@@ -29,10 +29,11 @@ export default async function Home() {
 
       <p className="w-[80ch] pb-10">
         Designmässigt är inspirationen tagen från passagerarlistor och andra
-        dokument från tiden mellan 1880-1930. Nicklas vill förmedla en känsla av
-        att man bläddrar i en sådan passagerarlista och då blir kontakten med
-        historian mer personlig. En lista rakt upp och ner hade aldrig varit
-        lika intressant att använda, det tror inte Nicklas i alla fall.
+        dokument från tiden mellan 1880&#x2010;1930. Nicklas vill förmedla en
+        känsla av att man bläddrar i en sådan passagerarlista och då blir
+        kontakten med historien mer personlig. En lista rakt upp och ner hade
+        aldrig varit lika intressant att använda, det tror inte Nicklas i alla
+        fall.
       </p>
       <Link
         className="border p-2 pt-3 hover:bg-green hover:text-basic-white"
