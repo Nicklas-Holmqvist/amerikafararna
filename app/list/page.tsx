33bcf0f32380643153bf84a84bf135bfad6bc127
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2024-04-26 16:56:13 +0200

    refactor: changed in meta description

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -5,7 +5,7 @@ import Table from '../components/Table';
 export const metadata: Metadata = {
   title: 'Markemigranter',
   description:
-    'Hitta alla som emigrerat eller immigrerat från Älekulla socken till Amerika mellan 1862 till 1928',
+    'Hitta de som emigrerat eller immigrerat från och till Älekulla socken från Amerika mellan 1862 till 1928',
 };
 
 export default async function Home() {
