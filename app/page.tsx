ab8e0c2074d85c7ad2e5b6a71966d9bc13d5b605
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2024-05-09 17:45:34 +0200

    feat: fix analytics data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next/types';
 
 import LandingPageStory from './components/LandingPageStory';
+import { supabase } from './lib/supabaseClient';
+
+import DummyData from '../data/dummy.json';
 
 export const metadata: Metadata = {
   title: 'Markemigranter | Markemigranter.se',
@@ -8,7 +11,153 @@ export const metadata: Metadata = {
     'Hitta alla som emigrerat eller immigrerat från Älekulla socken till Amerika mellan 1862 till 1928',
 };
 
+interface AnalyseData {
+  parish: string;
+  person_id: number;
+  year_of_birth: string;
+  age_when_emigration: number | null;
+  emigration_date: string | null;
+  age_when_immigration: number | null;
+  immigration_date: string | null;
+  gender_type: string;
+}
+
+async function getAnalyticData() {
+  const { data, error } = await supabase
+    .from('travellers')
+    .select(
+      'parish, person_id, year_of_birth, age_when_emigration, emigration_date, immigration_date, age_when_immigration, gender_type',
+      { count: 'exact' }
+    );
+
+  if (error) {
+    throw new Error('Failed to fetch data');
+  }
+
+  // const data = DummyData;
+
+  const emigrationTopChart = getEmigrationChartData(data);
+
+  const countOfEmigrants = data.filter((person) => {
+    if (person.emigration_date !== null) return person.emigration_date;
+  }).length;
+  const countOfImmigrants = data.filter((person) => {
+    if (person.immigration_date !== null) return person.immigration_date;
+  }).length;
+
+  const countOfGenders = {
+    males: data.filter((person) => person.gender_type === 'man').length,
+    womans: data.filter((person) => person.gender_type === 'kvinna').length,
+    boys: data.filter((person) => person.gender_type === 'pojke').length,
+    girls: data.filter((person) => person.gender_type === 'flicka').length,
+  };
+
+  const medianOfAges = {
+    total: getMedianAge(data, 'total'),
+    men: getMedianAge(data, 'man'),
+    women: getMedianAge(data, 'kvinna'),
+  };
+
+  const oldestByGender = {
+    men: getOldestByGender(data, 'man'),
+    woman: getOldestByGender(data, 'kvinna'),
+  };
+
+  return {
+    countOfEmigrants,
+    countOfImmigrants,
+    countOfGenders,
+    medianOfAges,
+    oldestByGender,
+    emigrationTopChart,
+  };
+}
+
+function getEmigrationChartData(travellers: AnalyseData[]) {
+  let travellersPerYear = [];
+  let years: string[] = [];
+
+  for (const year of travellers) {
+    if (year.emigration_date !== null)
+      years.push(year.emigration_date?.slice(0, 4));
+    else undefined;
+  }
+  const filterdYears = removeYearDoubles(years).sort();
+
+  for (const year of filterdYears) {
+    let count = 0;
+    for (const person of travellers) {
+      if (person.emigration_date !== null) {
+        const emigrationYear = person.emigration_date!.slice(0, 4);
+
+        if (emigrationYear === year) count += 1;
+      }
+    }
+    travellersPerYear.push({ year: year, travellers: count });
+  }
+  console.log(travellersPerYear);
+}
+
+function removeYearDoubles(data: string[]) {
+  return data.filter((year, index) => data.indexOf(year) === index);
+}
+
+function getOldestByGender(travellers: AnalyseData[], type: string) {
+  let getAges = [];
+  for (const person of travellers) {
+    if (type === 'man') {
+      if (person.age_when_emigration !== null && person.gender_type === 'man')
+        getAges.push(person.age_when_emigration);
+      else getAges.push(0);
+    }
+    if (type === 'kvinna') {
+      if (
+        person.age_when_emigration !== null &&
+        person.gender_type === 'kvinna'
+      )
+        getAges.push(person.age_when_emigration);
+      else getAges.push(0);
+    }
+  }
+  if (getAges === null) return getAges;
+  return Math.max(...getAges);
+}
+
+function getMedianAge(travellers: AnalyseData[], type: string) {
+  let count = 0;
+  let getAges = 0;
+  for (const person of travellers) {
+    if (type === 'total') {
+      if (person.age_when_emigration !== null) {
+        getAges += person.age_when_emigration;
+        count += 1;
+      } else getAges += 0;
+    }
+    if (type === 'man') {
+      if (
+        person.age_when_emigration !== null &&
+        (person.gender_type === 'man' || person.gender_type === 'pojke')
+      ) {
+        getAges += person.age_when_emigration;
+        count += 1;
+      } else getAges += 0;
+    }
+    if (type === 'kvinna') {
+      if (
+        person.age_when_emigration !== null &&
+        (person.gender_type === 'kvinna' || person.gender_type === 'flicka')
+      ) {
+        getAges += person.age_when_emigration;
+        count += 1;
+      } else getAges += 0;
+    }
+  }
+  return Math.round(getAges / count);
+}
+
 export default async function Home() {
+  const data: any = await getAnalyticData();
+
   return (
     <main className="flex flex-col items-center justify-between bg-schablon bg-repeat bg-small">
       <LandingPageStory />
